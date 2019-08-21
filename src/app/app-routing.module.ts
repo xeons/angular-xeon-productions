2a0068918bced7d10e47157dffc49d8d8b39059b
Brandon Scott <xeons@users.noreply.github.com>
2019-08-21 16:47:19 -0500

    Adding components, pages, and some assets.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FrontPageComponent } from './front-page/front-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'index', component: FrontPageComponent },
+  { path: '', redirectTo: '/index', pathMatch: 'full' },
+  { path: 'about', component: AboutPageComponent },
+  { path: '**', component: PageNotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { RightSidebarComponent } from './layout/right-sidebar/right-sidebar.component';
+import { NavigationComponent } from './layout/navigation/navigation.component';
+import { FrontPageComponent } from './front-page/front-page.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AboutPageComponent } from './about-page/about-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    RightSidebarComponent,
+    NavigationComponent,
+    FrontPageComponent,
+    PageNotFoundComponent,
+    AboutPageComponent
   ],
   imports: [
     BrowserModule,
